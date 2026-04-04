15e756b00b9a8f6dc6d787c5b31eb330749af8f1
arshmanahmad <arshmanmuhammad153@gmail.com>
2026-04-04 23:11:54 +0500

    feat: update

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -3,7 +3,6 @@ import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
 import AuthLayout from "../../components/Layout/AuthLayout";
 import { getApiErrorMessage } from "../../services/api";
-
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
