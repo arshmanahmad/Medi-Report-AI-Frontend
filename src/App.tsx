ec115c3047603ca4040aea283b8d4c06827a69af
arshmanahmad <arshmanmuhammad153@gmail.com>
2026-04-25 00:51:06 +0500

    feat: update

--- src/App.tsx
+++ src/App.tsx
@@ -111,6 +111,7 @@ function App() {
               </AdminRoute>
             }
           />
+
           {/* Legacy routes - redirect to new routes */}
           <Route
             path="/input"
